9118703a5ac39c024192f836bb007a50bd471a02
张俊 <jun24.zhang@tcl.com>
2020-09-22 16:32:11 +0800

    feat(继承): es5继承 静态属性以及 symbol 属性

--- src/inherit/inherit_es5_pro.js
+++ src/inherit/inherit_es5_pro.js
@@ -1,33 +1,60 @@
-function A2(name) {
+function A3(name) {
     this.name = name
     this.hobby = 'ball'
 }
 
-A2.prototype.showName = function () {
+A3.prototype.showName = function () {
     console.log('show Name ', this.name)
 }
 
-// A2 上的属性（理解为class中的静态属性
-A2.life = '100'
+// A3 上的属性（理解为class中的静态属性
+A3.life = '100'
 
-function B2(name, age) {
-    A2.call(this,name)
+function B3(name, age) {
+    A3.call(this,name)
     this.age = age
 }
 
-B2.prototype.showAge = function () {
+B3.prototype.showAge = function () {
     console.log('show Age ', this.age)
 }
 
-function extends(target,source){
+function extend(target,source){
     const targetType = typeof target
     const sourceType = typeof source
     if (targetType === 'undefined' || sourceType === 'undefined'
-        || targetType !== 'function' || source !== 'function'
+        || targetType !== 'function' || sourceType !== 'function'
     ) {
         console.warn('继承目标或者被继承目标只能是 function 类型')
         return null
     }
+    // 1. 继承静态属性（包括方法）
+    // 2. 继承 symbol 属性
+    // 3. 改变原型链指向
+    // 4. copy 原来原型上的属性方法
+    const srcProps = Object.getOwnPropertyNames(source)
+    const srcSymbols = Object.getOwnPropertySymbols(source)
+    const targetProtoPropNames = Object.getOwnPropertyNames(target.prototype)
+    const targetProtoProps = target.prototype
+
+
+    srcProps.length && srcProps.forEach(prop=>{
+        if (typeof target[prop] === 'undefined') {
+            target[prop] = source[prop]
+        }
+    })
+    srcSymbols.length && srcSymbols.forEach(symbol=>{
+        if (typeof target[symbol] === 'undefined') {
+            target[symbol] = source[symbol]
+        }
+    })
+
+    target.prototype = Object.create(source.prototype)
+    target.prototype.constructor = target
+
+    targetProtoPropNames.length && targetProtoPropNames.forEach(prop=>{
+        target.prototype[prop]=targetProtoProps[prop]
+    })
 }
 
-Object.setPrototypeOf(B2.prototype,A2.prototype)
+extend(B3,A3)
